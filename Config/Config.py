c546a856462ccd8e146bf841c3b3e13fee21d644
liutao <751226701@qq.com>
2024-01-31 16:44:04 +0800

    适配ZU15A版本

--- Config/Config.py
+++ Config/Config.py
@@ -10,7 +10,7 @@ import os
 
 class Config:
     # 项目地址
-    url = "http://192.168.21.82/#/login"
+    url = "http://192.168.21.115/#/login"
 
     # 项目根目录
     root_dir = os.path.split(os.path.split(__file__)[0])[0]
