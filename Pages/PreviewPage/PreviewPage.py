00d9b68e7cdfd4d0265f8405aa22fd0bfe7184de
liutao <751226701@qq.com>
2024-02-29 11:33:31 +0800

    增加回放管理用例

---
+++ Pages/PreviewPage/PreviewPage.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @author: 刘涛
+# @time: 2024/1/17 9:57
+# @file: PreviewPage.py
+# @project: ZS22A_UI
+
+import os.path
+import allure
+from time import sleep
+from Common.Common import Common
+from Config.Config import Config
+from Common.CompareImage import are_images_equal, download_image
+from playwright.sync_api import expect
+
+
+class PreviewPage(Common):
+    __video_start = ".el-tooltip"
+    __video_stop = ".el-tooltip"
+    __screenshot = "i:nth-child(2)"
+    __screenshot_close = ".el-icon-circle-close"
+    __start_record = ".btn-opr > i:nth-child(3)"
+    __stop_record = ".btn-opr > i:nth-child(3)"
+    __continuous_snapshot = "i:nth-child(4)"
+    __auto_focus = "li > .el-tooltip"
+    __electronic_amplification = ("list", "i")
+    __real_time_temp_curve = ".el-tooltip.icon-report"
+    __object_temp_select = ("请选择", 1)
+    __object_temp_max = "最高温"
+    __object_temp_min = "最低温"
+    __object_temp_avg = "平均温"
+    __time_range_select = "请选择"
+    __time_range_24H = "近24小时"
+    __time_range_72H = "近72小时"
+    __time_range_4H = "近4小时"
+    __fill_or_adapt = ".el-tooltip.icon-videomode"
+    __full_screen = ".el-tooltip.icon-full"
+    __vl_full_screen = ".video-wrap > canvas"
+    __ir_full_screen = ".show_window_ir > .video-wrap > canvas"
+    __full_screen_exit = ".video-wrap > canvas"
+    __full_screen_exit_ir = ".show_window_ir > .video-wrap > canvas"
+    __root = ("button", "root")
+    __logout = "退出登录"
+    __confirm_exit = ("button", "确认退出")
+
+    @allure.step("点击暂停播放")
+    def click_video_stop(self):
+        sleep(1)
+        self._click(self.__video_stop)
+
+    @allure.step("断言暂停播放")
+    def assert_video_stop(self, value):
+        expect(self.page.locator(value[0])).to_contain_text(value[1])
+
+    @allure.step("点击开始播放")
+    def click_video_start(self):
+        self._click(self.__video_stop)
+
+    @allure.step("断言开始播放")
+    def assert_video_start(self, value):
+        expect(self.page.locator(value[0])).to_contain_text(value[1])
+
+    @allure.step("点击抓图")
+    def click_screenshot(self):
+        self._click(self.__screenshot)
+
+    @allure.step("断言抓图成功")
+    def assert_click_screenshot(self, value):
+        expect(self.page.locator(value)).to_be_visible()
+
+    @allure.step("点击关闭抓图框")
+    def click_screenshot_close(self):
+        self._click(self.__screenshot_close)
+
+    @allure.step("断言关闭抓图框成功")
+    def assert_click_screenshot_close(self, value):
+        expect(self.page.locator(value)).to_be_hidden()
+
+    @allure.step("点击开始录制")
+    def click_start_record(self):
+        self._click(self.__start_record)
+
+    @allure.step("断言红外通道开始录制成功")
+    def assert_start_ir_record(self, value):
+        expect(self.page.locator(value[0]).first).to_be_visible()
+
+    @allure.step("断言可见光通道开始录制成功")
+    def assert_start_vl_record(self, value):
+        expect(self.page.locator(value[1]).first).to_be_visible()
+
+    @allure.step("点击停止录制")
+    def click_stop_record(self):
+        self._click(self.__stop_record)
+
+    @allure.step("断言红外通道已停止录制")
+    def assert_stop_ir_record(self, value):
+        expect(self.page.locator(value[0]).first).to_be_hidden()
+
+    @allure.step("断言可见光通道已停止录制")
+    def assert_stop_vl_record(self, value):
+        expect(self.page.locator(value[1]).first).to_be_hidden()
+
+    @allure.step("点击连续抓图")
+    def click_continuous_snapshot(self):
+        self._click(self.__continuous_snapshot)
+
+    @allure.step("断言连续抓图成功")
+    def assert_continuous_snapshot(self, value):
+        expect(self.page.get_by_text(value)).to_be_visible(timeout=300000)
+
+    @allure.step("点击自动聚焦")
+    def click_auto_focus(self):
+        self._click(self.__auto_focus)
+
+    @allure.step("断言自动聚焦成功")
+    def assert_auto_focus(self, value):
+        expect(self.page.get_by_text(value)).to_be_visible()
+
+    @allure.step("点击电子变倍")
+    def click_electronic_amplification(self):
+        self.page.get_by_role("list").locator("i").first.click()
+
+    @allure.step("点击实时温度曲线")
+    def click_real_time_temp_curve(self):
+        self._click(self.__real_time_temp_curve)
+
+    @allure.step("断言打开实时温度曲线成功")
+    def assert_real_time_temp_curve_start(self, value):
+        expect(self.page.locator(value)).to_be_visible()
+
+    @allure.step("点击选择对象温度")
+    def click_object_temp_select(self):
+        self.page.get_by_placeholder(self.__object_temp_select[0]).nth(self.__object_temp_select[1]).click()
+
+    @allure.step("选择对象温度最高温")
+    def select_object_temp_max(self):
+        self.page.get_by_text(self.__object_temp_max).click()
+
+    @allure.step("选择对象温度最低温")
+    def select_object_temp_mix(self):
+        self.page.get_by_text(self.__object_temp_min).click()
+
+    @allure.step("选择对象温度平均温")
+    def select_object_temp_avg(self):
+        self.page.get_by_text(self.__object_temp_avg).click()
+
+    @allure.step("断言选择对象温度成功")
+    def assert_select_object_temp_success(self, value):
+        expect(self.page.get_by_placeholder("请选择").nth(1)).to_have_value(value)
+
+    @allure.step("点击选择时间范围")
+    def click_time_range_select(self):
+        self.page.get_by_placeholder(self.__time_range_select).first.click()
+
+    @allure.step("选择时间范围4小时")
+    def select_time_range_4H(self):
+        self.page.get_by_text(self.__time_range_4H).click()
+
+    @allure.step("选择时间范围24小时")
+    def select_time_range_24H(self):
+        self.page.get_by_text(self.__time_range_24H).click()
+
+    @allure.step("选择时间范围72小时")
+    def select_time_range_72H(self):
+        self.page.get_by_text(self.__time_range_72H).click()
+
+    @allure.step("断言选择时间范围切换成功")
+    def assert_select_time_range_success(self, value):
+        expect(self.page.get_by_placeholder("请选择").first).to_have_value(value)
+
+    @allure.step("断言关闭实时温度曲线成功")
+    def assert_real_time_temp_curve_stop(self, value):
+        expect(self.page.locator(value)).to_be_hidden()
+
+    @allure.step("点击填充或适应")
+    def click_fill_or_adapt(self):
+        self._click(self.__fill_or_adapt)
+
+    @allure.step("断言填充或适应成功")
+    def assert_fill_or_adapt(self, value):
+        expect(self.page.get_by_text(value)).to_be_visible()
+
+    @allure.step("全屏显示")
+    def click_full_screen(self):
+        self._click(self.__full_screen)
+
+    @allure.step("可见光全屏显示")
+    def click_vl_full_screen(self):
+        self.page.locator(self.__vl_full_screen).first.dblclick(position={"x": 345, "y": 156})
+
+    @allure.step("红外全屏显示")
+    def click_ir_full_screen(self):
+        self.page.locator(self.__ir_full_screen).dblclick(position={"x": 293, "y": 236})
+
+    @allure.step("退出全屏显示")
+    def click_full_screen_exit(self):
+        self.page.locator(self.__full_screen_exit).first.dblclick()
+
+    @allure.step("退出单可见光全屏显示")
+    def click_vl_full_screen_exit(self):
+        self.page.locator(self.__full_screen_exit).first.dblclick()
+
+    @allure.step("退出单红外全屏显示")
+    def click_ir_full_screen_exit(self):
+        self.page.locator(self.__full_screen_exit_ir).first.dblclick()
+
+    @allure.step("点击root用户")
+    def click_root(self):
+        self.page.get_by_role("button", name="root").click()
+
+    @allure.step("退出登录")
+    def click_logout(self):
+        self.page.get_by_text(self.__logout).click()
+
+    @allure.step("确认退出")
+    def click_confirm_exit(self):
+        self.page.get_by_role("button", name="确认退出").click()
+
+    @allure.step("断言退出登录成功")
+    def assert_logout_success(self, value):
+        expect(self.page.get_by_placeholder(value)).to_be_visible()
---
+++ TestCases/test_case_03.py
@@ -0,0 +1,448 @@
+# ！/usr/bin/env python
+# -*- coding = utf-8 -*-
+# @Author : 刘涛
+# @Time : 2024/2/2 14:05
+# @File :test_case_03.py
+# @Project : ZS22A_UI
+
+import os
+import allure
+import pytest
+from playwright.sync_api import sync_playwright
+from Pages.PlaybackPage.PlaybackPage import PlaybackPage
+from Common.ReadYaml import ReadYaml
+from Common.AllurePretty import PrettyAllure
+from Config.Config import Config
+
+yaml_data = ReadYaml(os.path.join(Config.test_datas_dir, "test_data_03.yaml")).read()
+logindata = yaml_data[0]
+Trace = Config.trace3
+pageobject = None
+DOWNLOAD_FLAG = False
+
+@allure.step("登录")
+def login(pageobject, url, user, passwd):
+    pageobject.goto(url)
+    pageobject.get_by_placeholder("请输入用户名").fill(user)
+    pageobject.get_by_placeholder("请输入密码").fill(passwd)
+    pageobject.wait_for_timeout(3000)
+    pageobject.get_by_role("button", name="登录").click()
+    pageobject.get_by_text("回放管理").click()
+def on_download(download):
+    global DOWNLOAD_FLAG
+    DOWNLOAD_FLAG = True
+    download.save_as(os.path.join(Config.test_download_dir, download.suggested_filename))
+def set_download_flag(flag):
+    global DOWNLOAD_FLAG
+    DOWNLOAD_FLAG = flag
+
+@pytest.fixture(scope="class")
+def page():
+    global pageobject
+    with sync_playwright() as play:
+        browser = play.chromium.launch(
+            headless=False,
+            channel=Config.browser,
+            args=['--start-maximized'],
+            slow_mo=500)
+
+        context = browser.new_context(no_viewport=True)
+        if Trace:
+            context.tracing.start(screenshots=True, snapshots=True, sources=True)
+        else:
+            pass
+        if pageobject is None:
+            pageobject = context.new_page()
+            try:
+                pageobject.on('download', on_download)
+            except Exception as e:
+                print(f"保存失败：{e}")
+            login(pageobject, logindata["url地址"], logindata["账号"], logindata["密码"])
+        yield pageobject
+        pageobject = None
+        if Trace:
+            context.tracing.stop(path="trace3.zip")
+        else:
+            pass
+        context.close()
+        browser.close()
+
+"""执行回放管理模块测试"""
+class TestPlayBack:
+
+    """搜索全部可见光视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[1]])
+    def test_case_01(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.click_query()
+        page.assert_num_list(CaseData['断言元素定位'])
+
+    """搜索对象温度可见光视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[2]])
+    def test_case_02(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_event_type()
+        page.select_object_temp()
+        page.click_query()
+        page.assert_num_list(CaseData['断言元素定位'])
+
+    """搜索全局温度可见光视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[3]])
+    def test_case_03(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_event_type()
+        page.select_global_temp()
+        page.click_query()
+        page.assert_num_list(CaseData['断言元素定位'])
+
+    """搜索对象温差可见光视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[4]])
+    def test_case_04(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_event_type()
+        page.select_object_temp_dif()
+        page.click_query()
+        page.assert_num_list(CaseData['断言元素定位'])
+
+    """搜索手动录像视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[5]])
+    def test_case_05(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_event_type()
+        page.select_manual_recording()
+        page.click_query()
+        page.assert_num_list(CaseData['断言元素定位'])
+
+    """搜索全部红外视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[6]])
+    def test_case_06(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+        page.assert_num_list(CaseData['断言元素定位'])
+
+    """搜索对象温度红外视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[7]])
+    def test_case_07(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_object_temp()
+        page.click_query()
+        page.assert_num_list(CaseData['断言元素定位'])
+
+    """搜索全局温度红外视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[8]])
+    def test_case_08(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_global_temp()
+        page.click_query()
+        page.assert_num_list(CaseData['断言元素定位'])
+
+    """搜索对象温差红外视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[9]])
+    def test_case_09(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_object_temp_dif()
+        page.click_query()
+        page.assert_num_list(CaseData['断言元素定位'])
+
+    """下载回放视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[10]])
+    def test_case_10(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+        page.download_playback_video()
+        assert DOWNLOAD_FLAG == CaseData['断言元素定位']
+        set_download_flag(False)
+
+    """搜索全部可见光照片"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[11]])
+    def test_case_11(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_image()
+        page.select_channel_vl()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+
+    """搜索对象温度可见光照片"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[12]])
+    def test_case_12(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_image()
+        page.select_channel_vl()
+        page.select_event_type()
+        page.select_object_temp()
+        page.click_query()
+
+    """搜索全局温度可见光照片"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[13]])
+    def test_case_13(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_image()
+        page.select_channel_vl()
+        page.select_event_type()
+        page.select_global_temp()
+        page.click_query()
+
+    """搜索对象温差可见光照片"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[14]])
+    def test_case_14(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_image()
+        page.select_channel_vl()
+        page.select_event_type()
+        page.select_object_temp_dif()
+        page.click_query()
+
+    """搜索全部红外照片"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[15]])
+    def test_case_15(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_image()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+
+    """搜索对象温度红外照片"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[16]])
+    def test_case_16(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_image()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_object_temp()
+        page.click_query()
+
+    """搜索全局温度红外照片"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[17]])
+    def test_case_17(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_image()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_global_temp()
+        page.click_query()
+
+    """搜索对象温差红外照片"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[18]])
+    def test_case_18(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_image()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_object_temp_dif()
+        page.click_query()
+
+    """下载回放照片"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[19]])
+    def test_case_19(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_image()
+        page.select_channel_vl()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+        page.download_playback_picture()
+        assert DOWNLOAD_FLAG == CaseData['断言元素定位']
+        set_download_flag(False)
+
+    """选择第一条视频播放"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[20]])
+    def test_case_20(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_video()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+        page.select_video(1)  # 选择第一个视频
+        page.page.wait_for_timeout(10000)
+        page.assert_selected_video1(CaseData['断言元素定位'])
+
+    """选择下一条视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[21]])
+    def test_case_21(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_video()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+        page.select_video(1)  # 选择第一个视频
+        page.next_video()
+        page.assert_selected_video2(CaseData['断言元素定位'])
+
+    """选择上一条视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[22]])
+    def test_case_22(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_video()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+        page.select_video(2)  # 选择第一个视频
+        page.previous_video()
+        page.assert_selected_video1(CaseData['断言元素定位'])
+
+    """停止播放视频"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[23]])
+    def test_case_23(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_video()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+        page.select_video(1)
+        page.page.wait_for_timeout(5000)
+        page.stop_play_video()
+
+    """视频倍速1.2X"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[24]])
+    def test_case_24(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_video()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+        page.select_magnification()
+        page.magnification_2()
+        page.assert_magnification(CaseData['断言元素定位'])
+
+    """视频倍速1.5X"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[25]])
+    def test_case_25(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_video()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+        page.select_magnification()
+        page.magnification_3()
+        page.assert_magnification(CaseData['断言元素定位'])
+
+    """视频倍速2.0X"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[26]])
+    def test_case_26(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_video()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+        page.select_magnification()
+        page.magnification_4()
+        page.assert_magnification(CaseData['断言元素定位'])
+
+    """视频倍速1.0X"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[27]])
+    def test_case_27(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_video()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+        page.select_magnification()
+        page.magnification_1()
+        page.assert_magnification(CaseData['断言元素定位'])
+
+    """全屏播放"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[28]])
+    def test_case_28(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_video()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_all_documents()
+        page.click_query()
+        page.full_screen_play()
+        page.assert_full_screen_status(CaseData['断言元素定位'])
+
+    """退出全屏播放"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[29]])
+    def test_case_29(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.stop_full_screen_play()
+        page.assert_full_screen_status(CaseData['断言元素定位'])
+
+    """照片播放后暂停"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[30]])
+    def test_case_30(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.select_file_type_image()
+        page.select_channel_ir()
+        page.select_event_type()
+        page.select_global_temp()
+        page.click_query()
+        page.play_photos()
+        page.page.wait_for_timeout(10000)
+        page.assert_selected_photo(CaseData['断言元素定位'])
+        page.stop_play_photos()
+
+    """下一张照片"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[31]])
+    def test_case_31(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.next_photo()
+        page.assert_selected_photo(CaseData['断言元素定位'])
+
+    """上一张照片"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[32]])
+    def test_case_32(self, page, CaseData: dict):
+        page = PlaybackPage(page)
+        page.previous_photo()
+        page.assert_selected_photo(CaseData['断言元素定位'])
+
+
+
+
+
+
