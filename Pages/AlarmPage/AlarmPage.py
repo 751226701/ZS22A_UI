9c18355f68cbebb740a7468be9f290b2f6c7e8be
liutao <751226701@qq.com>
2024-03-04 13:32:45 +0800

    增加报警管理用例

--- Pages/AlarmPage/AlarmPage.py
+++ Pages/AlarmPage/AlarmPage.py
@@ -4,3 +4,18 @@
 # @Time : 2024/3/4 13:25
 # @File :AlarmPage.py
 # @Project : ZS22A_UI
+
+from PIL import Image
+import io
+import base64
+import os.path
+import allure
+from time import sleep
+from Common.Common import Common
+from Config.Config import Config
+from Common.CompareImage import are_images_equal, download_image
+from playwright.sync_api import expect
+
+class AlarmPage(Common):
+    # 元素定位器
+    __username = '.el-input__inner[placeholder="请输入用户名"]'
--- TestCases/test_case_05.py
+++ TestCases/test_case_05.py
@@ -4,3 +4,74 @@
 # @Time : 2024/3/4 13:25
 # @File :test_case_05.py
 # @Project : ZS22A_UI
+
+import os
+import allure
+import pytest
+from playwright.sync_api import sync_playwright
+from Pages.AlarmPage.AlarmPage import AlarmPage
+from Common.ReadYaml import ReadYaml
+from Common.AllurePretty import PrettyAllure
+from Config.Config import Config
+
+yaml_data = ReadYaml(os.path.join(Config.test_datas_dir, "test_data_05.yaml")).read()
+logindata = yaml_data[0]
+Trace = Config.trace5
+pageobject = None
+DOWNLOAD_FLAG = False
+
+@allure.step("登录")
+def login(pageobject, url, user, passwd):
+    pageobject.goto(url)
+    pageobject.get_by_placeholder("请输入用户名").fill(user)
+    pageobject.get_by_placeholder("请输入密码").fill(passwd)
+    pageobject.wait_for_timeout(3000)
+    pageobject.get_by_role("button", name="登录").click()
+    pageobject.get_by_text("报警管理").click()
+def on_download(download):
+    global DOWNLOAD_FLAG
+    DOWNLOAD_FLAG = True
+    download.save_as(os.path.join(Config.test_download_dir, download.suggested_filename))
+def set_download_flag(flag):
+    global DOWNLOAD_FLAG
+    DOWNLOAD_FLAG = flag
+
+@pytest.fixture(scope="class")
+def page():
+    global pageobject
+    with sync_playwright() as play:
+        browser = play.chromium.launch(
+            headless=False,
+            channel=Config.browser,
+            args=['--start-maximized'],
+            slow_mo=500)
+
+        context = browser.new_context(no_viewport=True)
+        if Trace:
+            context.tracing.start(screenshots=True, snapshots=True, sources=True)
+        else:
+            pass
+        if pageobject is None:
+            pageobject = context.new_page()
+            try:
+                pageobject.on('download', on_download)
+            except Exception as e:
+                print(f"保存失败：{e}")
+            login(pageobject, logindata["url地址"], logindata["账号"], logindata["密码"])
+        yield pageobject
+        pageobject = None
+        if Trace:
+            context.tracing.stop(path="trace3.zip")
+        else:
+            pass
+        context.close()
+        browser.close()
+
+"""执行统计报表模块测试"""
+class TestAlarm:
+
+    """查看所有分析对象最高温温度曲线"""
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[1]])
+    def test_case_01(self, page, CaseData: dict):
+        page = AlarmPage(page)
