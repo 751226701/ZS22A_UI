6ecb817b5708c370a8b17b85e323e0452aa76984
liutao <751226701@qq.com>
2024-03-19 10:44:15 +0800

    增加画面设置子模块元素封装

---
+++ Pages/SystemPage/SystemPage1.py
@@ -0,0 +1,211 @@
+# ！/usr/bin/env python
+# -*- coding = utf-8 -*-
+# @Author : 刘涛
+# @Time : 2024/3/19 10:36
+# @File :SystemPage1
+# @Project : ZS22A_UI
+
+from PIL import Image
+import io
+import re
+import base64
+import os.path
+import allure
+from time import sleep
+from Common.Common import Common
+from Config.Config import Config
+from Common.CompareImage import are_images_equal, download_image
+from playwright.sync_api import expect
+
+class SystemPage(Common):
+    # 画面设置子模块元素定位
+    __test = ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
