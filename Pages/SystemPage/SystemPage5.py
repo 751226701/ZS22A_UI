80f70e7c726d5a98326c07699bd5ff12685aff2e
liutao <751226701@qq.com>
2024-03-19 16:47:32 +0800

    增加图像融合子模块元素封装

---
+++ Pages/SystemPage/SystemPage5.py
@@ -0,0 +1,183 @@
+# ！/usr/bin/env python
+# -*- coding = utf-8 -*-
+# @Author : 刘涛
+# @Time : 2024/3/19 15:43
+# @File :SystemPage5
+# @Project : ZS22A_UI
+
+from PIL import Image
+import io
+import re
+import base64
+import os.path
+import allure
+from time import sleep
+from Common.Common import Common
+from Config.Config import Config
+from Common.CompareImage import are_images_equal, download_image
+from playwright.sync_api import expect
+
+class SystemPage(Common):
+    # 全局测温设置子模块元素定位
+    __test = ""
+
+    @allure.step("点击鼠标显示温度开关")
+    def click_mouse_temp_switch(self):
+        self.page.locator("div").filter(has_text=re.compile(r"^鼠标点击显示温度$")).locator("span").click()
+
+    @allure.step("点击测温档位下拉框")
+    def click_temp_level_select(self):
+        self.page.locator("div").filter(has_text=re.compile(r"^测温档位 ℃$|^-20~150100~550 ℃$")).get_by_placeholder("请选择").click()
+
+    @allure.step("选择-20-150度档位")
+    def select_150(self):
+        self.page.get_by_text("-20~").click()
+
+    @allure.step("选择100-550度档位")
+    def select_550(self):
+        self.page.get_by_text("~550").click()
+
+    @allure.step("选择色带")
+    def select_color_bar(self, value):
+        self.page.get_by_label("全局设置").locator("i").nth(2).click()
+        self.page.get_by_text(value).click()
+
+    @allure.step("点击色带显示开关")
+    def click_color_bar_switch(self):
+        self.page.locator("div").filter(has_text=re.compile(r"^色带条显示$")).locator("span").click()
+
+    @allure.step("点击冷热点追踪开关")
+    def click_hot_cold_switch(self):
+        (self.page.get_by_label("全局设置").locator("form div").
+         filter(has_text="冷热点追踪设置 高温 清空 确定 低温 清空 确定").get_by_role("switch").locator("span").click())
+
+    @allure.step("设置自定义发射率值")
+    def set_emiss(self, value):
+        self.page.get_by_label("全局温度参数").get_by_role("textbox").nth(1).fill(value)
+
+    @allure.step("点击发射率下拉框")
+    def click_emiss_select(self):
+        self.page.get_by_label("全局温度参数").get_by_placeholder("请选择").click()
+
+    @allure.step("选择发射率")
+    def select_emiss(self, value):
+        self.click_emiss_select()
+        self.page.get_by_text(value).click()
+
+    @allure.step("设置距离参数")
+    def set_distance(self, value):
+        (self.page.get_by_label("全局温度参数").locator("form div").
+         filter(has_text="距离 M (0.1~200)").get_by_role("textbox").fill(value))
+
+    @allure.step("设置反射温度")
+    def set_reflect(self, value):
+        (self.page.locator("div").filter(has_text=re.compile(r"^反射温度 ℃ \(-40~2000\)$")).
+         get_by_role("textbox").fill(value))
+
+    @allure.step("设置全局温度修正值")
+    def set_isotherm(self, value):
+        (self.page.get_by_label("全局温度参数").locator("form div").
+         filter(has_text="全局温度修正 ℃(-1.0~1.0)").get_by_role("textbox").fill(value))
+
+    @allure.step("点击等温线开关")
+    def click_isotherm_switch(self):
+        self. page.get_by_label("等温线设置").get_by_role("switch").locator("span").click()
+
+    @allure.step("选择关闭等温线")
+    def close_isotherm(self):
+        self.page.get_by_label("等温线设置").locator("i").first.click()
+
+    @allure.step("设置高等温线")
+    def set_isotherm_high(self):
+        self.page.get_by_label("等温线设置").locator("i").nth(1).click()
+
+    @allure.step("设置低等温线")
+    def set_isotherm_low(self):
+        self.page.get_by_label("等温线设置").locator("i").nth(2).click()
+
+    @allure.step("设置区间等温线")
+    def set_isotherm_range(self):
+        self.page.get_by_label("等温线设置").locator("i").nth(3).click()
+
+    @allure.step("设置区间外等温线")
+    def set_isotherm_range_out(self):
+        self.page.get_by_label("等温线设置").locator("i").nth(4).click()
+
+    @allure.step("设置等温线值高温阈值")
+    def set_isotherm_high_value(self, value):
+        (self.page.get_by_label("等温线设置").locator("form div").
+         filter(has_text="高温 ℃").get_by_role("textbox").fill(value))
+
+    @allure.step("设置等温线值低温阈值")
+    def set_isotherm_low_value(self, value):
+        (self.page.get_by_label("等温线设置").locator("form div").
+         filter(has_text="低温 ℃ 清空 确定").get_by_role("textbox").fill(value))
+
+    @allure.step("点击默认")
+    def click_default(self):
+        self.page.get_by_role("button", name="默认").click()
+
+    @allure.step("点击刷新")
+    def click_refresh(self):
+        self.page.get_by_role("button", name="刷新").click()
+
+    @allure.step("点击确定")
+    def click_ok(self):
+        self.page.get_by_role("button", name="确定").click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- TestCases/test_case_17.py
+++ TestCases/test_case_17.py
@@ -1,8 +1,8 @@
 # ！/usr/bin/env python
 # -*- coding = utf-8 -*-
 # @Author : 刘涛
-# @Time : 2024/3/19 15:38
-# @File :test_case_16
+# @Time : 2024/3/19 15:43
+# @File :test_case_17
 # @Project : ZS22A_UI
 
 import os
@@ -10,14 +10,14 @@ import re
 import allure
 import pytest
 from playwright.sync_api import sync_playwright
-from Pages.SystemPage.SystemPage4 import SystemPage
+from Pages.SystemPage.SystemPage5 import SystemPage
 from Common.ReadYaml import ReadYaml
 from Common.AllurePretty import PrettyAllure
 from Config.Config import Config
 
-yaml_data = ReadYaml(os.path.join(Config.test_datas_dir, "test_data_16.yaml")).read()
+yaml_data = ReadYaml(os.path.join(Config.test_datas_dir, "test_data_17.yaml")).read()
 logindata = yaml_data[0]
-Trace = Config.trace16
+Trace = Config.trace17
 pageobject = None
 DOWNLOAD_FLAG = False
 
@@ -29,7 +29,8 @@ def login(pageobject, url, user, passwd):
     pageobject.wait_for_timeout(3000)
     pageobject.get_by_role("button", name="登录").click()
     pageobject.get_by_text("系统管理").click()
-    pageobject.get_by_role("menuitem", name="图像融合").click()
+    pageobject.get_by_text("测温设置", exact=True).click()
+    pageobject.get_by_role("menuitem", name="全局测温设置").click()
 def on_download(download):
     global DOWNLOAD_FLAG
     DOWNLOAD_FLAG = True
@@ -63,41 +64,44 @@ def page():
         yield pageobject
         pageobject = None
         if Trace:
-            context.tracing.stop(path="trace16.zip")
+            context.tracing.stop(path="trace17.zip")
         else:
             pass
         context.close()
         browser.close()
 
-"""执行图像融合子模块测试"""
+"""执行全局测温设置子模块测试"""
 class TestAlarm:
 
-    """设置图像融合"""
+    """设置鼠标点击温度显示"""
     @PrettyAllure.PrettyAllureWarpper
     @pytest.mark.parametrize("CaseData", [yaml_data[1]])
     def test_case_01(self, page, CaseData: dict):
         page = SystemPage(page)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        page.click_mouse_temp_switch()
+        page.click_temp_level_select()
+        page.select_550()
+        page.select_color_bar("黑热")
+        page.select_color_bar("彩虹1")
+        page.select_color_bar("人体筛查")
+        page.select_color_bar("医疗A")
+        page.click_color_bar_switch()
+        page.click_hot_cold_switch()
+        page.set_emiss("0.5")
+        page.select_emiss("不锈钢")
+        page.select_emiss("石膏")
+        page.select_emiss("铜板")
+        page.set_distance("9")
+        page.set_reflect("999")
+        page.set_isotherm("0.5")
+        page.click_isotherm_switch()
+        page.close_isotherm()
+        page.set_isotherm_high()
+        page.set_isotherm_low()
+        page.set_isotherm_range()
+        page.set_isotherm_range_out()
+        page.set_isotherm_high_value("88")
+        page.set_isotherm_low_value("88")
 
 
 
