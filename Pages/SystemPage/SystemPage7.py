f4b2d4580afc0f580a6e884aaabf075af23ff6aa
liutao <751226701@qq.com>
2024-03-20 09:11:22 +0800

    增加补光灯子模块元素封装

--- Pages/SystemPage/SystemPage7.py
+++ Pages/SystemPage/SystemPage7.py
@@ -1,8 +1,8 @@
 # ！/usr/bin/env python
 # -*- coding = utf-8 -*-
 # @Author : 刘涛
-# @Time : 2024/3/19 17:17
-# @File :SystemPage
+# @Time : 2024/3/20 8:50
+# @File :SystemPage7
 # @Project : ZS22A_UI
 
 from PIL import Image
@@ -22,21 +22,25 @@ class SystemPage(Common):
     # 屏蔽区域子模块元素定位
     __test = ""
 
-    @allure.step("添加分析对象")
-    def add_obj(self, x, y, x1, y1):
-        """
-        :param x: 起始横坐标
-        :param y: 起始纵坐标
-        :param x1: 末端横坐标
-        :param y1: 末端纵坐标
-        :return: 无
-        """
-        self.page.locator("form i").first.click()
-        mouse = self.page.mouse
-        mouse.move(x, y)
-        mouse.down()
-        mouse.move(x1, y1, steps=50)
-        mouse.up()
+    @allure.step("补光灯模式选择")
+    def select_light_mode(self):
+        self.page.get_by_placeholder("请选择").click()
+
+    @allure.step("手动")
+    def select_manual(self):
+        self.page.get_by_text("手动").click()
+
+    @allure.step("时间自动")
+    def select_time_auto(self):
+        self.page.get_by_text("时间自动").click()
+
+    @allure.step("补光灯开关")
+    def select_light_switch(self):
+        self.page.get_by_role("switch").locator("span").click()
+
+    @allure.step("补光灯亮度设置")
+    def select_light_brightness(self, value):
+        self.page.locator("form div").filter(has_text="补光灯亮度").get_by_role("textbox").fill(value)
 
     @allure.step("点击默认")
     def click_default(self):
@@ -49,4 +53,36 @@ class SystemPage(Common):
     @allure.step("点击确定")
     def click_ok(self):
         self.page.get_by_role("button", name="确定").click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ TestCases/test_case_19.py
@@ -0,0 +1,105 @@
+# ！/usr/bin/env python
+# -*- coding = utf-8 -*-
+# @Author : 刘涛
+# @Time : 2024/3/20 8:50
+# @File :test_case_19
+# @Project : ZS22A_UI
+
+import os
+import re
+import allure
+import pytest
+from playwright.sync_api import sync_playwright
+from Pages.SystemPage.SystemPage7 import SystemPage
+from Common.ReadYaml import ReadYaml
+from Common.AllurePretty import PrettyAllure
+from Config.Config import Config
+
+yaml_data = ReadYaml(os.path.join(Config.test_datas_dir, "test_data_19.yaml")).read()
+logindata = yaml_data[0]
+Trace = Config.trace19
+pageobject = None
+DOWNLOAD_FLAG = False
+
+@allure.step("登录")
+def login(pageobject, url, user, passwd):
+    pageobject.goto(url)
+    pageobject.get_by_placeholder("请输入用户名").fill(user)
+    pageobject.get_by_placeholder("请输入密码").fill(passwd)
+    pageobject.wait_for_timeout(3000)
+    pageobject.get_by_role("button", name="登录").click()
+    pageobject.get_by_text("系统管理").click()
+    pageobject.get_by_text("外设管理", exact=True).click()
+    pageobject.get_by_role("menuitem", name="补光灯").click()
+def on_download(download):
+    global DOWNLOAD_FLAG
+    DOWNLOAD_FLAG = True
+    download.save_as(os.path.join(Config.test_download_dir, download.suggested_filename))
+def set_download_flag(flag):
+    global DOWNLOAD_FLAG
+    DOWNLOAD_FLAG = flag
+
+@pytest.fixture(scope="class")
+def page():
+    global pageobject
+    with sync_playwright() as play:
+        browser = play.chromium.launch(
+            headless=False,
+            channel=Config.browser,
+            args=['--start-maximized'],
+            slow_mo=500)
+
+        context = browser.new_context(no_viewport=True)
+        if Trace:
+            context.tracing.start(screenshots=True, snapshots=True, sources=True)
+        else:
+            pass
+        if pageobject is None:
+            pageobject = context.new_page()
+            try:
+                pageobject.on('download', on_download)
+            except Exception as e:
+                print(f"保存失败：{e}")
+            login(pageobject, logindata["url地址"], logindata["账号"], logindata["密码"])
+        yield pageobject
+        pageobject = None
+        if Trace:
+            context.tracing.stop(path="trace19.zip")
+        else:
+            pass
+        context.close()
+        browser.close()
+
+
+"""执行补光灯子模块测试"""
+
+class TestAlarm:
+    """设置补光灯"""
+
+    @PrettyAllure.PrettyAllureWarpper
+    @pytest.mark.parametrize("CaseData", [yaml_data[1]])
+    def test_case_01(self, page, CaseData: dict):
+        page = SystemPage(page)
+        page.select_light_mode()
+        page.select_time_auto()
+        page.select_light_mode()
+        page.select_manual()
+        page.select_light_switch()
+        page.select_light_brightness("80")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
