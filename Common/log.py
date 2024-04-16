89b384529f31b2490eb2e76f2ab2e79e370ef624
liutao <751226701@qq.com>
2024-04-16 10:49:12 +0800

    bug修复

--- Common/log.py
+++ Common/log.py
@@ -1,16 +1,9 @@
-# ！/usr/bin/env python
-# -*- coding = utf-8 -*-
-# @Author : 刘涛
-# @Time : 2024/1/20 14:55
-# @File :log.py
-# @Software : PyCharm
-
-
 import os
 import logging
 import inspect
 import colorlog
 from Config.Config import Config
+from logging.handlers import RotatingFileHandler
 
 LOG_PATH = Config.logs    # 日志路径
 PREFIX_NAME = ""          # 日志前缀名
@@ -19,49 +12,57 @@ LOG_ERROR = "error.log"   # 日志名称
 
 
 class Logger:
-    def __init__(self):
+    def __init__(self, console_output=True, file_output=True):
+        """
+        :param console_output: 是否输出到控制台
+        :param file_output: 是否写入到日志文件
+        """
         if os.path.exists(LOG_PATH):
             pass
         else:
             os.mkdir(LOG_PATH)
         self.info_logger = logging.getLogger("info")  # 创建info级别日志记录器
         self.error_logger = logging.getLogger("error")  # 创建error级别日志记录器
-        self.format = logging.Formatter('[%(asctime)s][%(levelname)-8s] - %(message)s')  # 格式化输出
+        self.format = logging.Formatter('[%(asctime)s][%(levelname)s] - %(message)s')  # 格式化输出
         # 指定文件位置文件名以及输出格式
-        info_file_handler = logging.FileHandler("%s/%s%s" % (LOG_PATH, PREFIX_NAME, LOG_INFO))
-        info_file_handler.setFormatter(self.format)
-        error_file_handler = logging.FileHandler("%s/%s%s" % (LOG_PATH, PREFIX_NAME, LOG_ERROR))
-        error_file_handler.setFormatter(self.format)
+        if file_output:
+            info_file_handler = RotatingFileHandler(
+                "%s/%s%s" % (LOG_PATH, PREFIX_NAME, LOG_INFO),
+                maxBytes=1024 * 1024 * 10,  # 每个日志文件最大10MB
+                backupCount=5  # 保留5个旧日志文件
+            )
+            info_file_handler.setFormatter(self.format)
+
+            error_file_handler = RotatingFileHandler(
+                "%s/%s%s" % (LOG_PATH, PREFIX_NAME, LOG_ERROR),
+                maxBytes=1024 * 1024 * 10,  # 每个日志文件最大10MB
+                backupCount=5  # 保留5个旧日志文件
+            )
+            error_file_handler.setFormatter(self.format)
+
+            self.info_logger.addHandler(info_file_handler)
+            self.error_logger.addHandler(error_file_handler)
         # 创建控制台处理器,将日志同步输出到控制台
-        console_handler = logging.StreamHandler()
-        console_formatter = colorlog.ColoredFormatter(
-            '%(log_color)s[%(asctime)s][%(levelname)-8s] - %(message)s',
-            log_colors={
-                'DEBUG': 'cyan',
-                'INFO': 'green',
-                'WARNING': 'yellow',
-                'ERROR': 'red',
-                'CRITICAL': 'bold_red',
-            }
-        )
-        console_handler.setFormatter(console_formatter)
-        # 将处理器添加到日志记录器中
-        self.info_logger.addHandler(info_file_handler)
-        self.error_logger.addHandler(error_file_handler)
-        self.info_logger.addHandler(console_handler)  # 添加控制台处理器
-        self.error_logger.addHandler(console_handler)  # 添加控制台处理器
+        if console_output:
+            console_handler = logging.StreamHandler()
+            console_formatter = colorlog.ColoredFormatter(
+                '%(log_color)s[%(asctime)s][%(levelname)s] - %(message)s',
+                log_colors={
+                    'DEBUG': 'cyan',
+                    'INFO': 'green',
+                    'WARNING': 'yellow',
+                    'ERROR': 'red',
+                    'CRITICAL': 'bold_red',
+                }
+            )
+            console_handler.setFormatter(console_formatter)
+            self.info_logger.addHandler(console_handler)  # 添加控制台处理器
+            self.error_logger.addHandler(console_handler)  # 添加控制台处理器
         # 指定日志的最低输出级别
         self.info_logger.setLevel(logging.DEBUG)
         self.error_logger.setLevel(logging.ERROR)
 
     def debug(self, msg, *args, **kwargs):
-        """
-        记录调试级别的日志消息
-        :param msg:要记录的消息
-        :param args:传递给日志函数的额外位置参数
-        :param kwargs:传递给日志函数的额外关键字参数
-        :return:
-        """
         caller_frame = inspect.currentframe().f_back
         caller_filename = os.path.basename(caller_frame.f_code.co_filename)
         caller_lineno = caller_frame.f_lineno
@@ -95,24 +96,6 @@ class Logger:
         caller_lineno = caller_frame.f_lineno
         message = f"{caller_filename}:{caller_lineno} - {msg}"
         self.error_logger.critical(message, *args, **kwargs)
-
-    def ASSERT_EQ(self, value, expect):
-        """
-        断言是否相等
-        :param value: 实际值
-        :param expect: 期望值
-        :return: 无
-        """
-        caller_frame = inspect.currentframe().f_back
-        caller_filename = os.path.basename(caller_frame.f_code.co_filename)
-        caller_lineno = caller_frame.f_lineno
-        message = f"{caller_filename}:{caller_lineno}"
-        try:
-            assert value == expect
-        except AssertionError:
-            self.error_logger.error(f"{message}-{value}不等于{expect}")
-        else:
-            self.info_logger.info(f"{message}-{value}等于{expect}")
 
 
 if __name__ == "__main__":
